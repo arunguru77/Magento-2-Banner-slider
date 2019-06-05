e8718186b62d7ee9ced5dcd8e4b9d799329191b8
Arun Kumar Tyagi <arunkumartyagi424@gmail.com>
2019-06-05 13:27:50 +0530

    initial code

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,18 @@
+
+/**
+ * Magento 2 owlcarousel
+ *  
+ */
+
+var config = {
+    "map": {
+        "*": {
+            "OwlCarousel": "Arun_BannerSlider/js/owlcarousel",
+            "OwlCarousel-min": "Arun_BannerSlider/js/owl.carousel.min"
+        }
+    },
+    "shim":{
+        "Arun_BannerSlider/js/owl.carousel.min": ["jquery"],
+        "Arun_BannerSlider/js/owlcarousel": ["jquery"]
+    }
+};
